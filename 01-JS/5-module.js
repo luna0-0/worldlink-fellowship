bb6bba2a9c4d9fa0aabe5237340b507d7b904927
luna <lunadahal77@gmail.com>
2025-08-12 23:12:52 +0545

    software-fellowship day-1 javascript

--- 01-JS/5-module.js
+++ 01-JS/5-module.js
@@ -1,19 +1,20 @@
-const path=require("path");
+const path = require("path");
 
-const currentPath=path.join(__dirname);
-console.log("cirrentPath",currentPath);
+const currentPath = path.join(__dirname);
+console.log("Current Path is: ", currentPath);
+// console.log(__dirname);
 
-const fs=require("fs");
+const fs = require("fs");
 
-console.info(`Reading file from ${currentPath}`);
-const data=fs.readFileSync(path.join(currentPath, "./1.js"),"utf-8");
+console.info(`Reading file from ${path.join(currentPath, "./1.js")}`);
+const data = fs.readFileSync(path.join(currentPath, "./1.js"), "utf-8");
+console.log(data);
 
-fs.writeFileSync(path.join(currentPath,"./op.txt"),"update from code");
+fs.writeFileSync(path.join(currentPath, "./op.txt"), "update from code");
 
-console.log("data");
-
-
-const os=require("os");
+const os = require("os");
+console.log(os.type());
+console.log(os.arch());
 console.log(os.platform());
 console.log(os.freemem());
 console.log(os.userInfo());
